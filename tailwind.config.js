b3dc3dccbfe6ec444a40bb36fb2380c6ab51a160
akikukeo <165561768+akikukeo@users.noreply.github.com>
2025-01-07 13:09:54 +0000

    ESLintとPrettierの設定ファイルを修正、スタイルシートの整形、フッターコンポーネントの変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-// tailwind.config.js
 module.exports = {
   content: [
-    // './src/**/*.{html,js}',
-      'node_modules/preline/dist/*.js',
-  ],
+    // This is the important part
+    './src/**/*.{html,js,ts,jsx,tsx,vue}',
+    'node_modules/preline/dist/*.js',
+  ], 
+  // darkMode for 'class'
+  darkMode: "class",
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+  },
   plugins: [
     // require('@tailwindcss/forms'),
-      require('preline/plugin'),
+    require('preline/plugin'),
   ],
 }
